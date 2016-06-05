468e8be201fb4bb33b3e111b6ac7e949058d0dc7
Casey Chad Salvador <salvador.caseychad@gmail.com>
2016-06-04 22:30:27 -0400

    Letter Game
    Created a guessing letter game.

---
+++ Python/Apps/random_letter_game_01.py
@@ -0,0 +1,63 @@
+#Random Letter Game
+import random
+
+# make a list of words
+words = [
+    'blue',
+    'red',
+    'green',
+    'purple',
+    'black',
+    'white',
+    'gray',
+    'pink',
+    'orange'
+]
+
+while True:
+    start = input("Press enter/return to start, or enter Q to quit: ")
+    if start.lower() == 'q':
+        break
+    
+    # pick a random word
+    secret_word = random.choice(words) # picks random word out of iterable
+    bad_guesses = []
+    good_guesses = []
+
+    while len(bad_guesses) < 7 and len(good_guesses) != len(list(secret_word)):
+        # draw spaces
+        # draw guessed letters and strikes
+        for letter in secret_word:
+            if letter in good_guesses:
+                print(letter, end='')
+            else:
+                print('_ ', end='')
+        print('')
+        print('Strikes: {}/7'.format(len(bad_guesses)))
+        print('')
+        
+        # take guess
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1:
+            print("You can only guess a single letter!")
+            continue
+        elif guess in bad_guesses or guess in good_guesses:
+            print("You've already guess that letter!")
+            contiue
+        elif not guess.isalpha():
+            print("You can only guess letters!")
+            continue
+
+        if guess in secret_word:
+            good_guesses.append(guess)
+            if len(good_guesses) == len(list(secret_word)):
+                print("You win! The word was {}".format(secret_word))
+                break
+        else:
+            bad_guesses.append(guess)
+            
+    else:
+        print("You didn't guess it! My secret word was {}".format(secret_word))
+    # print out win/lose
+
