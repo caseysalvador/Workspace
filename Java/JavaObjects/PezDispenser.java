4eaaee55162a24ba234d4f2013f6383c96d8a795
Casey Chad Salvador <salvador.caseychad@gmail.com>
2017-01-02 15:06:39 -0500

    Java Constants Team Treehouse Example
    working with Java Constants.

--- Java/JavaObjects/PezDispenser.java
+++ Java/JavaObjects/PezDispenser.java
@@ -3,6 +3,8 @@
  */
 
 class PezDispenser {
+    public static final int MAX_PEZ = 12;
+
     final private String characterName;
 
     // constructor same name as class
--- Java/JavaObjects/pezExample.java
+++ Java/JavaObjects/pezExample.java
@@ -3,11 +3,12 @@
  */
 public class pezExample {
 
-    public static void main(String[] args) {
-        System.out.println("We are making a new PEZ Dispenser");
-        PezDispenser dispenser = new PezDispenser("Yoda");
-        System.out.printf("The dispenser is %s %n",
-                dispenser.getCharacterName()
-        );
-    }
+  public static void main(String[] args) {
+      System.out.println("We are making a new PEZ Dispenser");
+      System.out.printf("FUN FACT: There are %d PEZ in every dispenser %n",
+              PezDispenser.MAX_PEZ);
+      PezDispenser dispenser = new PezDispenser("Yoda");
+      System.out.printf("The dispenser is %s %n",
+              dispenser.getCharacterName());
+  }
 }
