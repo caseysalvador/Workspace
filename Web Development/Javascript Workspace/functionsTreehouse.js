39f51c0a8a6f20d10785c5ebe33985905e17facf
Casey Chad Salvador <salvador.caseychad@gmail.com>
2017-01-30 21:36:13 -0500

    Created openJS.html
    openJS allows you to open a JS file and run it within your web browser.

---
+++ Web Development/Javascript Workspace/functionsTreehouse.js
@@ -0,0 +1,13 @@
+// functions
+function alertRandom() {
+  var randomNumber = Math.floor() * 6 ) + 1;
+  alert(randomNumber);
+}
+alertRandom();
+
+// function expression
+var alertRandom1 = function(){
+  var randomNumber1 = Math.floor() * 6 ) + 1;
+  alert(randomNumber1);
+};
+alertRandom1();
