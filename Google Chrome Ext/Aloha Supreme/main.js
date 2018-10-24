d28b3fbdc77b1237c1831b6ce21c7bcc49972216
Casey Salvador <salvador.caseychad@gmail.com>
2018-10-24 13:28:09 -0400

    Working on main.js for Aloha Supreme

---
+++ Google Chrome Ext/Aloha Supreme/main.js
@@ -0,0 +1,4 @@
+function openSettings() {
+   document.getElementById("settings").classList.toggle("settings-open");
+   document.getElementById("settings-button").addEventListener('click', openSettings);
+}
